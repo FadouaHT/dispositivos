534ca22027a6a5f03374027188999abaf67456af
FadouaHT <hathouti_f246@ieseduardoprimo.es>
2024-02-27 20:20:54 +0100

    1

---
+++ dispositivosAF/src/main/java/Dispositivo.java
@@ -0,0 +1,14 @@
+import java.time.LocalDate;
+
+public class Dispositivo {
+    //Atributos: Identificador, FechaCompra, Precio, Tipo Atributo, Marca, Modelo,
+   private String id;
+   private LocalDate fechaCompra;
+
+
+    //Métodos : AltaDispositivo(), BajaDispositivo(), ModificarDispositivo().
+    // Pantalla: De tipo modal para Alta/Modificación dispositivos.
+
+
+
+}
